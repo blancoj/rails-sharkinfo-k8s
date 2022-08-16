0ecbb54527d947169a8da2f2885cefae95e1b4b5
Seth Johnson <sethajoh@umich.edu>
2022-08-16 13:49:38 -0400

    check_yarn_integrity=false for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # weird k8s sidekiq error
+  config.webpacker.check_yarn_integrity = false
 end
